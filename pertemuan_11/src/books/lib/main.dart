ca0e97c1c2038a09797b045c6b9a59605c58ebb2
Filla Ramadhani <fillau123@gmail.com>
2024-11-22 00:38:46 +0700

    W11: Soal 12

--- pertemuan_11/src/books/lib/main.dart
+++ pertemuan_11/src/books/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'geolocation.dart'; // Mengimpor file geolocation.dart
 
 void main() {
   runApp(const MyApp());
@@ -10,65 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Error Handling Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const FuturePage(),
-    );
-  }
-}
-
-class FuturePage extends StatefulWidget {
-  const FuturePage({super.key});
-
-  @override
-  State<FuturePage> createState() => _FuturePageState();
-}
-
-class _FuturePageState extends State<FuturePage> {
-  String result = '';
-
-  // Method yang dapat melempar error
-  Future returnError() async {
-    await Future.delayed(const Duration(seconds: 2));
-    throw Exception('Something terrible happened!');
-  }
-
-  // Menangani error menggunakan try-catch-finally
-  Future handleError() async {
-    try {
-      await returnError(); // Memanggil method yang dapat melempar error
-    } catch (error) {
-      setState(() {
-        result = error.toString(); // Menampilkan pesan error
-      });
-    } finally {
-      print('Complete'); // Menampilkan 'Complete' ke konsol
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Error Handling Example')),
-      body: Center(
-        child: Column(
-          children: [
-            const Spacer(),
-            ElevatedButton(
-              child: const Text('Trigger Error'),
-              onPressed: () {
-                handleError(); // Memanggil method handleError saat tombol ditekan
-              },
-            ),
-            const Spacer(),
-            Text(result), // Menampilkan hasil error atau pesan
-            const Spacer(),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false, // Menghilangkan banner debug
+      title: 'Geolocation App by Filla',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home:
+          const LocationScreen(), // Panggil LocationScreen sebagai layar utama
     );
   }
 }
