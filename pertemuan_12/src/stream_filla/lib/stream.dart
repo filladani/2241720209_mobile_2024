6e07c090ee1c1565018f3a0fe035e96a2efd8d46
Filla Ramadhani <fillau123@gmail.com>
2024-11-28 08:05:09 +0700

    W12: Jawaban Soal 3

--- pertemuan_12/src/stream_filla/lib/stream.dart
+++ pertemuan_12/src/stream_filla/lib/stream.dart
@@ -21,6 +21,24 @@ class ColorStream {
   // Getter untuk stream
   Stream<Color> get stream => _streamController.stream;
 
+  // Method untuk menghasilkan warna sebagai stream (menggunakan generator)
+  Stream<Color> getColors() async* {
+    // Perulangan manual dengan jeda
+    for (Color color in colors) {
+      await Future.delayed(const Duration(seconds: 1)); // Delay 1 detik
+      yield color; // Kirim warna ke stream
+    }
+
+    // Menggunakan yield* dengan Stream.periodic
+    yield* Stream.periodic(
+      const Duration(seconds: 1),
+      (int t) {
+        int index = t % colors.length; // Index berulang
+        return colors[index]; // Warna berdasarkan index
+      },
+    );
+  }
+
   // Function untuk menambahkan warna baru ke stream
   void addColor(Color color) {
     _streamController.add(color);
